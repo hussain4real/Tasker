389a84c97852056c9351bb2ec0a33cdb4a7262df
Hussain Aminu <hussain_aminu@yahoo.com>
2022-12-31 23:15:45 +0300

    Implemented the drag and drop feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { CdkDragDrop, transferArrayItem, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tasker';
+  
+  todo: Task[] = [
+    {
+      title: 'Task 1',
+      description: 'Go to the store and buy milk',
+},
+    {
+      title: 'Task 2',
+      description: 'Go to the store and buy bread',
+},
+  ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    )
+    // moveItemInArray(this.todo, event.previousIndex,event.currentIndex)
+  }
 }
